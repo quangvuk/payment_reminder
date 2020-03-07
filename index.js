85a5e81d7debe20b5c84b31638bb593a142b4d51
quang vu <quangvuk@outlook.com>
2020-03-07 22:57:34 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,127 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+// Sendgrid
+const key = require('./key/key');
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(key.SENDGRID_API_KEY);
+
+// Node-cron schedule task
+const cron = require('node-cron');
+var task=cron.schedule('* * * * *', () => {
+    console.log('running a task sending email every minute');
+    //reminderSystem();
+});
+
+// Body Parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Cloud Firestore
+const admin = require('firebase-admin');
+let serviceAccount = require('./key/bill-reminder-84699-firebase-adminsdk-xyysr-dfd724b8dc.json');
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+let db = admin.firestore();
+
+// View engine PUG
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+
+// Sending email
+function sendEmail(amount, notedDate){
+    let msg = {
+        to: 'kvkuypatckrdafbjlk@ttirv.net',
+        from: 'quangvuk@outlook.com',
+        subject: 'Bill Payment Reminder',
+        text: 'Make payment for your bill please!',
+        html: '<strong>Amount...' + amount + '....Noted Date...' + notedDate + '....</strong>',
+      };
+      sgMail.send(msg).then(val => {
+        console.log('sent successfully');
+      }).catch(err => {
+        console.log('failed');
+      });
+      return msg;
+}
+
+function reminderSystem(){
+    db.collection('reminder_list').get()
+        .then((snapshot) => {
+            snapshot.forEach((doc) => {
+                if (!doc.data().IsDone) {
+                    let data = doc.data();
+                    sendEmail(data.Amount, data.NotedDate);
+                }
+            });
+        })
+        .catch((err) => {
+            console.log('Error getting documents', err);
+        }); 
+    
+}
+
+
+app.get('/', (req, res) => {
+    let users = [];
+    db.collection('users').get()
+        .then((snapshot) => {
+            snapshot.forEach((doc) => {
+                let user = {
+                    id: doc.id,
+                    data: doc.data()
+                }
+                users.push(user);
+                //console.log(doc.id, '=>', doc.data());
+            });
+            console.log(users);
+            res.render('assign_page',{users});
+        })
+        .catch((err) => {
+            console.log('Error getting documents', err);
+        }); 
+});
+
+app.get('/management', (req, res) => {
+    let users = [];
+    db.collection('users').get()
+        .then((snapshot) => {
+            snapshot.forEach((doc) => {
+                users.push(doc.data());
+                
+            });
+            console.log(users);
+            res.render('management_page',{users});
+        })
+        .catch((err) => {
+            console.log('Error getting documents', err);
+        }); 
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    
+    let reminder = db.collection('reminder_list').doc();
+
+    let setReminder = reminder.set({
+        'Amount':req.body.amount,
+        'Created': Date(Date.now()),
+        'LastReminder': Date(Date.now()),
+        'NotedDate':req.body.date,
+        'UserId': req.body.user,
+        'PhoneReminder': req.body.phone==='true'?true:false,
+        'SlackReminder': req.body.slack==='true'?true:false,
+        'EmailReminder': req.body.email==='true'?true:false,
+        'Interval': req.body.interval,
+        'Description': req.body.description,
+        'IsDone':false
+    });
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.listen(port, () => console.log(`Running on port ${port}`));
