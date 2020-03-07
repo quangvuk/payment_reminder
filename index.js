a2aa0b43ac53e12827741c3a0a207f78325b5328
quang vu <quangvuk@outlook.com>
2020-03-08 02:13:43 +0900

    update: management page

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ sgMail.setApiKey(key.SENDGRID_API_KEY);
 
 // Node-cron schedule task
 const cron = require('node-cron');
-var task=cron.schedule('* * * * *', () => {
+var task = cron.schedule('* * * * *', () => {
     console.log('running a task sending email every minute');
     //reminderSystem();
 });
 
 // Body Parser
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Cloud Firestore
 const admin = require('firebase-admin');
@@ -33,23 +33,23 @@ app.set('views', './views');
 
 
 // Sending email
-function sendEmail(amount, notedDate){
+function sendEmail(amount, notedDate) {
     let msg = {
         to: 'kvkuypatckrdafbjlk@ttirv.net',
         from: 'quangvuk@outlook.com',
         subject: 'Bill Payment Reminder',
         text: 'Make payment for your bill please!',
         html: '<strong>Amount...' + amount + '....Noted Date...' + notedDate + '....</strong>',
-      };
-      sgMail.send(msg).then(val => {
+    };
+    sgMail.send(msg).then(val => {
         console.log('sent successfully');
-      }).catch(err => {
+    }).catch(err => {
         console.log('failed');
-      });
-      return msg;
+    });
+    return msg;
 }
 
-function reminderSystem(){
+function reminderSystem() {
     db.collection('reminder_list').get()
         .then((snapshot) => {
             snapshot.forEach((doc) => {
@@ -61,8 +61,8 @@ function reminderSystem(){
         })
         .catch((err) => {
             console.log('Error getting documents', err);
-        }); 
-    
+        });
+
 }
 
 
@@ -79,28 +79,50 @@ app.get('/', (req, res) => {
                 //console.log(doc.id, '=>', doc.data());
             });
             console.log(users);
-            res.render('assign_page',{users});
+            res.render('assign_page', { users });
         })
         .catch((err) => {
             console.log('Error getting documents', err);
-        }); 
+        });
 });
 
 app.get('/management', (req, res) => {
     let reminders = [];
     db.collection('reminder_list').get()
         .then((snapshot) => {
+            let index = 0;
             snapshot.forEach((doc) => {
-                reminders.push(doc.data());
-                
+                let tempData = doc.data();
+                tempData.Index = index++;
+                tempData.Id = doc.id;
+                // let user = db.collection('users').doc(tempData.UserId);
+                // let getUser = user.get().then(val => {
+                //     tempData.UserName = val;
+                // }).catch(err => {
+                //     tempData.UserName = 'Not found';
+                //     console.log('Failed to find user!');
+                // })
+                reminders.push(tempData);
+
             });
             console.log(reminders);
-            res.render('management_page',{reminders});
+            res.render('management_page', { reminders });
         })
         .catch((err) => {
             console.log('Error getting documents', err);
-        }); 
+        });
 })
+
+app.get('/management/:reminderId', (req, res) => {
+    let reminderId = req.params.reminderId;
+
+    db.collection("reminder_list").doc(reminderId).update({
+        IsDone: true
+    }).then(val => {
+        res.redirect('/management');
+    });
+
+});
 
 
 app.get('/user/create', (req, res) => {
@@ -108,23 +130,34 @@ app.get('/user/create', (req, res) => {
 });
 
 app.post('/', (req, res) => {
-    console.log(req.body);
-    let reminder = db.collection('reminder_list').doc();
+    //console.log(req.body);
+    db.collection("users").doc(req.body.user)
+        .get()
+        .then(function (doc) {
+            let name = '';
+            if (doc.exists) {
+                name = doc.data().Name;
+            } 
 
-    let setReminder = reminder.set({
-        'Amount':req.body.amount,
-        'Created': Date(Date.now()),
-        'LastReminder': Date(Date.now()),
-        'NotedDate':req.body.date,
-        'UserId': req.body.user,
-        'PhoneReminder': req.body.phone==='true'?true:false,
-        'SlackReminder': req.body.slack==='true'?true:false,
-        'EmailReminder': req.body.email==='true'?true:false,
-        'Interval': req.body.interval,
-        'Description': req.body.description,
-        'IsDone':false
-    });
-    console.log(req.body);
+            let reminder = db.collection('reminder_list').doc();
+                let setReminder = reminder.set({
+                    'Amount': req.body.amount,
+                    'Created': Date(Date.now()),
+                    'LastReminder': Date(Date.now()),
+                    'NotedDate': req.body.date,
+                    'UserId': req.body.user,
+                    'UserName': name,
+                    'PhoneReminder': req.body.phone === 'true' ? true : false,
+                    'SlackReminder': req.body.slack === 'true' ? true : false,
+                    'EmailReminder': req.body.email === 'true' ? true : false,
+                    'Interval': req.body.interval,
+                    'Description': req.body.description,
+                    'IsDone': false
+                });
+        }).catch(function (error) {
+            console.log("Error getting document USER:", error);
+        });
+    //console.log(req.body);
     res.redirect('/');
 });
 
@@ -138,9 +171,10 @@ app.post('/user/create', (req, res) => {
         'SlackId': req.body.slackId
     });
 
-    newUser.then( val => {
+    newUser.then(val => {
         console.log("Create new user successfully!");
-    }).catch( err => {
+        res.redirect('/user/create');
+    }).catch(err => {
         console.log("Failed to create new user")
     });
 });
